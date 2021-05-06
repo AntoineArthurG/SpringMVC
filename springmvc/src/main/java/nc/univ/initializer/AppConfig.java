68ee2c20bc0507a34f649fd028986a64bf17eb3a
Antoine Arthur <71214784+AntoineArthurG@users.noreply.github.com>
2021-05-06 15:38:34 +1100

    appConfig update

--- springmvc/src/main/java/nc/univ/initializer/AppConfig.java
+++ springmvc/src/main/java/nc/univ/initializer/AppConfig.java
@@ -1,8 +1,12 @@
 package nc.univ.initializer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * AppConfig
@@ -12,5 +16,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "nc.univ")
 public class AppConfig {
 
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views");
+        viewResolver.setSuffix(".jsp");
+        
+        return viewResolver;
+    }
+    
     
 }
